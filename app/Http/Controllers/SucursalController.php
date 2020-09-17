79e1789e44e51b7df3da47c3efb86459a11f474a
francocava <francocavallini@hotmail.com>
2020-09-17 12:35:11 -0300

    empiezo con las rutas y controllers

---
+++ app/Http/Controllers/SucursalController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sucursal;
+use Illuminate\Http\Request;
+
+class SucursalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $sucursal = new Sucursal;
+
+        $sucursal->nombre = $request->nombre;
+        $sucursal->save(); //esto persiste en la db pero como sabe que es mi tabla sucursal?
+
+        return response()->json($sucursal); //para que es esto ultimo?? para que devuelva lo que meti?
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sucursal  $sucursal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sucursal $sucursal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sucursal  $sucursal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sucursal $sucursal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sucursal  $sucursal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sucursal $sucursal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sucursal  $sucursal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sucursal $sucursal)
+    {
+        //
+    }
+}
