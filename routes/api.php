73ab662d3e351c11879508528916786c494ef87e
Agustin Amato <agustin.amato96@gmail.com>
2020-11-07 20:37:14 -0300

    ajustes menores

--- routes/api.php
+++ routes/api.php
@@ -22,15 +22,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('/cliente','ClienteController');
-Route::apiResource('/obraSocial','ObraSocialController');
-Route::apiResource('/rol','RolController');
-Route::apiResource('/sucursal','SucursalController');
-Route::apiResource('/usuario','UsuarioController');
-Route::apiResource('/proveedor','ProveedorController');
-Route::apiResource('/accesorio','AccesorioController');
-Route::apiResource('/producto','ProductoController');
-Route::apiResource('/formaPago','FormaPagoController');
-Route::apiResource('/pago','PagoController');
-Route::apiResource('/cobro','CobroController');
-Route::apiResource('/pedido','PedidoController');
+Route::apiResource('/cliente', 'ClienteController');
+Route::apiResource('/obraSocial', 'ObraSocialController');
+Route::apiResource('/rol', 'RolController');
+Route::apiResource('/sucursal', 'SucursalController');
+Route::apiResource('/usuario', 'UsuarioController');
+Route::apiResource('/proveedor', 'ProveedorController');
+Route::apiResource('/accesorio', 'AccesorioController');
+Route::apiResource('/producto', 'ProductoController');
+Route::apiResource('/formaPago', 'FormaPagoController');
+Route::apiResource('/pago', 'PagoController');
+Route::apiResource('/cobro', 'CobroController');
+Route::apiResource('/pedido', 'PedidoController');
