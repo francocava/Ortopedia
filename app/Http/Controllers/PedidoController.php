098f563eb62e6356951dcb6e6ec63172362b51a9
francocava <francocavallini@hotmail.com>
2020-11-08 22:20:43 -0300

    detalle del controller

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -18,7 +18,7 @@ class PedidoController extends Controller
      */
     public function index()
     {
-        return response()->json(Pedido::with(['cliente:id,obra_id','usuario:id,usuario','sucursal'])->get());
+        return response()->json(Pedido::with(['cliente:id,obra_id,nombre,apellido','usuario:id,usuario','sucursal'])->get());
     }
 
     /**
