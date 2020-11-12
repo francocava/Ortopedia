594bd1074502517cc45e08b105b64d6917ca7885
Agustin Amato <agustin.amato96@gmail.com>
2020-11-12 17:30:57 -0300

    corrijo api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\PedidoController;
-use App\Http\Controllers\UsuarioController;
-use App\Pedido;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::post('/token', 'AuthController@login')->name('login');
+Route::post('/token', 'AuthController@login')->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', 'AuthController@me')->name('me');
@@ -38,19 +34,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/pedido', 'PedidoController');
     Route::apiResource('/pedidoItem', 'PedidoItemController');
     Route::apiResource('/factura','FacturaController');
-}); */
-
-Route::apiResource('/cliente', 'ClienteController');
-    Route::apiResource('/obraSocial', 'ObraSocialController');
-    Route::apiResource('/rol', 'RolController');
-    Route::apiResource('/sucursal', 'SucursalController');
-    Route::apiResource('/usuario', 'UsuarioController');
-    Route::apiResource('/proveedor', 'ProveedorController');
-    Route::apiResource('/accesorio', 'AccesorioController');
-    Route::apiResource('/producto', 'ProductoController');
-    Route::apiResource('/formaPago', 'FormaPagoController');
-    Route::apiResource('/pago', 'PagoController');
-    Route::apiResource('/cobro', 'CobroController');
-    Route::apiResource('/pedido', 'PedidoController');
-    Route::apiResource('/pedidoItem', 'PedidoItemController');
-    Route::apiResource('/factura','FacturaController');
+});
