5f9db0833f0336bbae7583a31a7ec6accb3251fd
Agustin Amato <agustin.amato96@gmail.com>
2020-11-17 19:04:30 -0300

    restauro rutas

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*
 Route::post('/token', 'AuthController@login')->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -36,19 +35,3 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/pedidoItem', 'PedidoItemController');
     Route::apiResource('/factura','FacturaController');
 });
-*/
-
-Route::apiResource('/cliente', 'ClienteController');
-Route::apiResource('/obraSocial', 'ObraSocialController');
-Route::apiResource('/rol', 'RolController');
-Route::apiResource('/sucursal', 'SucursalController');
-Route::apiResource('/usuario', 'UsuarioController');
-Route::apiResource('/proveedor', 'ProveedorController');
-Route::apiResource('/accesorio', 'AccesorioController');
-Route::apiResource('/producto', 'ProductoController');
-Route::apiResource('/formaPago', 'FormaPagoController');
-Route::apiResource('/pago', 'PagoController');
-Route::apiResource('/cobro', 'CobroController');
-Route::apiResource('/pedido', 'PedidoController');
-Route::apiResource('/pedidoItem', 'PedidoItemController');
-Route::apiResource('/factura','FacturaController');
