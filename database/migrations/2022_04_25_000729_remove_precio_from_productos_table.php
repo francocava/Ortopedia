2d2a79d45c85d99db2fdba6f0f639b3d6f499f1f
francocava <francocavallini@hotmail.com>
2022-04-25 20:04:56 -0300

    Ahora los importes pueden ser nros mas grandes

---
+++ database/migrations/2022_04_25_000729_remove_precio_from_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemovePrecioFromProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            $table->dropColumn('precio');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            $table->float('precio', 8, 2)->nullable();
+            //vuelve al formato anterior
+        });
+    }
+}
---
+++ database/migrations/2022_04_25_223245_add_precio_item_to_pedido_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPrecioItemToPedidoItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pedido_items', function (Blueprint $table) {
+            $table->float('precio_item', 13, 4)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pedido_items', function (Blueprint $table) {
+            $table->float('precio_item', 8, 2)->nullable();
+        });
+    }
+}
